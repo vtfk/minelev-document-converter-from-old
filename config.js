e434c7cef610c8cb109f83f0aa95d3b10c7f6d28
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-29 09:13:22 +0100

    Uptil now

---
+++ config.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || development,
+  MONGODB_CONNECTION: process.env.MONGODB_CONNECTION || null,
+  MONGODB_COLLECTION_NEW: process.env.MONGODB_COLLECTION_NEW || '',
+  MONGODB_NAME_NEW: process.env.MONGODB_NAME_NEW || '',
+  MONGODB_COLLECTION_OLD: process.env.MONGODB_COLLECTION_OLD || '',
+  MONGODB_NAME_OLD: process.env.MONGODB_NAME_OLD || ''
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+(async () => {
+  const { MONGODB_COLLECTION_OLD, MONGODB_COLLECTION_NEW, MONGODB_NAME_OLD, MONGODB_NAME_NEW } = require('./config')
+  const { logger } = require('@vtfk/logger')
+  const mongo = require('./lib/get-mongo')
+  const convert = require('./lib/convert-from-old')
+  const args = process.argv.slice(2)
+
+  // get documents from old db
+  const oldDb = await mongo(MONGODB_COLLECTION_OLD, MONGODB_NAME_OLD)
+
+  if (!oldDb) {
+    logger('error', [MONGODB_COLLECTION_OLD, MONGODB_NAME_OLD, 'not connected'])
+    process.exit(1)
+  }
+
+  // Get all documents? Or just the ones given on cli
+  let documents = []
+  if (args.length > 0) {
+    logger('info', ['index', "Henter angitte dokument id'er", args.length])
+    documents = await oldDb.find({ _id: { $in: args } }).sort({ _id: 1 }).toArray()
+  } else {
+    logger('info', ['index', 'Henter ALLE dokumenter'])
+    //documents = await oldDb.find({}).sort({ _id: 1 }).toArray()
+    documents = await oldDb.find({ _id: '5efde18c3cf2fd0008aaef4d' }).sort({ _id: 1 }).toArray()
+  }
+
+  logger('info', ['index', 'dokumenter', documents.length])
+
+  const converted = await convert(documents)
+  
+  process.exit(0)
+
+  /*const newDb = await mongo(MONGODB_COLLECTION_NEW, MONGODB_NAME_NEW)
+  if (!newDb || !newDb.isConnected) {
+    logger('error', [MONGODB_COLLECTION_NEW, MONGODB_NAME_NEW, 'not connected'])
+    process.exit(1)
+  }*/
+})()

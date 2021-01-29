4b384c26933f7ae54e6ac19137c5497b48b7580e
Mats Andreassen <matsand@live.no>
2021-01-29 23:38:47 +0100

    La til config av yff-collection

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   MONGODB_CONNECTION: process.env.MONGODB_CONNECTION || null,
   MONGODB_COLLECTION_NEW: process.env.MONGODB_COLLECTION_NEW || '',
+  MONGODB_COLLECTION_NEW_YFF: process.env.MONGODB_COLLECTION_NEW_YFF || '',
   MONGODB_NAME_NEW: process.env.MONGODB_NAME_NEW || '',
   MONGODB_COLLECTION_OLD: process.env.MONGODB_COLLECTION_OLD || '',
   MONGODB_NAME_OLD: process.env.MONGODB_NAME_OLD || ''

a3fd696b96557baa508703b0d058f5fd25a3fb59
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-29 15:36:23 +0100

    Add ObjectId to ids gotten from cli

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   const { MONGODB_COLLECTION_OLD, MONGODB_COLLECTION_NEW, MONGODB_NAME_OLD, MONGODB_NAME_NEW } = require('./config')
   const { logger } = require('@vtfk/logger')
   const mongo = require('./lib/get-mongo')
+  const { ObjectId } = require('mongodb')
   const convert = require('./lib/convert-from-old')
   const args = process.argv.slice(2)
 
@@ -17,11 +18,12 @@
   let documents = []
   if (args.length > 0) {
     logger('info', ['index', "Henter angitte dokument id'er", args.length])
-    documents = await oldDb.find({ _id: { $in: args } }).sort({ _id: 1 }).toArray()
+    const objIdDocuments = args.map(id => ObjectId(id))
+    documents = await oldDb.find({ _id: { $in: objIdDocuments } }).sort({ _id: 1 }).toArray()
   } else {
     logger('info', ['index', 'Henter ALLE dokumenter'])
     //documents = await oldDb.find({}).sort({ _id: 1 }).toArray()
-    documents = await oldDb.find({ _id: '5efde18c3cf2fd0008aaef4d' }).sort({ _id: 1 }).toArray()
+    documents = await oldDb.find({ _id: ObjectId('5efde18c3cf2fd0008aaef4d') }).sort({ _id: 1 }).toArray()
   }
 
   logger('info', ['index', 'dokumenter', documents.length])

70210a0bf32c3e5c2e4c8543bb7ad6066098532c
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-30 19:43:28 +0100

    Initial commit for insertOne til mongo db

---
+++ insert.js
@@ -0,0 +1,22 @@
+(async () => {
+  const { MONGODB_COLLECTION_OLD, MONGODB_NAME_OLD } = require('./config')
+  const { logger } = require('@vtfk/logger')
+  const mongo = require('./lib/get-mongo')
+  const { ObjectId } = require('mongodb')
+
+  // get documents from old db
+  const oldDb = await mongo(MONGODB_COLLECTION_OLD, MONGODB_NAME_OLD)
+
+  if (!oldDb) {
+    logger('error', [MONGODB_COLLECTION_OLD, MONGODB_NAME_OLD, 'not connected'])
+    process.exit(1)
+  }
+
+  // put your document here
+  const document = {}
+
+  const result = await oldDb.insertOne(document)
+  logger('info', ['index', 'dokumenter sendt ut', result.insertedCount])
+
+  process.exit(0)
+})()
